022b531f4de8e127c62164214866114216fc315f
4kict <4kict@bk.ru>
2017-03-06 00:21:04 +0300

    utf8mb4

--- src/main/java/gr/ru/HashMapDB.java
+++ src/main/java/gr/ru/HashMapDB.java
@@ -1,6 +1,5 @@
 package gr.ru;
 
-import com.google.common.base.Joiner;
 import gr.ru.dao.User;
 import io.netty.channel.Channel;
 import org.apache.log4j.LogManager;
@@ -23,11 +22,14 @@ public class HashMapDB {
     public HashMapDB() {
         BotBuilder botBuilder = new BotBuilder();
         add(botBuilder.setCountry("China").setCity("bejin").setLat(45261479).setLon(127754462).setName("bot1-Ch").build());
-        add(botBuilder.setCountry("Shri-Lanka").setCity("Bangalor").setLat(10848524).setLon(81078301).setName("bot1-Srialn").build());
-        add(botBuilder.setCountry("India").setCity("XZZ").setLat(25261066).setLon(304905461).setName("bot-Eg").build());
+        add(botBuilder.setCountry("Shri-Lanka").setCity("Bangalor").setLat(12942364).setLon(7767026).setName("bot1-Srialn").build());
+        add(botBuilder.setCountry("India").setCity("XZZ").setLat(28.6682049).setLon(77.2119786).setName("bot-Eg").build());
         add(botBuilder.setCountry("JUAR").setCity("Keiptaun").setLat(-27906751).setLon(21498461).setName("bot-juar").build());
         add(botBuilder.setCountry("Urugvai").setCity("Montevideo").setLat(-34543937).setLon(-56973623).setName("bot2").build());
-        add(botBuilder.setCountry("Panama").setCity("panama").setLat(8305348).setLon(-78822657).setName("bot3").build());
+        add(botBuilder.setCountry("Panama").setCity("panama").setLat(9015195).setLon(-79530845).setName("bot3").build());
+        add(botBuilder.setCountry("Australia").setCity("sidney").setLat(-33.848673).setLon(151.034710).setName("Sidne").build());
+        add(botBuilder.setCountry("USA").setCity("Torento").setLat(25.499860).setLon(-103.370393).setName("Sidne").build());
+
         add(botBuilder.setCountry("France").setCity("Pari").setLat(48854386).setLon(2319386).setName("bot4").build());
         add(botBuilder.setCountry("France").setCity("Pari").setLat(48870796).setLon(2306236).setName("bot5").build());
         add(botBuilder.setCountry("France").setCity("Pari").setLat(48804386).setLon(2339386).setName("bot6").build());
@@ -35,7 +37,9 @@ public class HashMapDB {
         add(botBuilder.setCountry("France").setCity("Pari").setLat(48864386).setLon(2355386).setName("bot8").build());
         add(botBuilder.setCountry("France").setCity("Pari").setLat(48874386).setLon(2369386).setName("bot9").build());
         add(botBuilder.setCountry("France").setCity("Pari").setLat(48884386).setLon(2374386).setName("bot10").build());
-        add(botBuilder.setCountry("France").setCity("Pari").setLat(48894386).setLon(2383386).setName("bot1").build());
+        add(botBuilder.setCountry("France").setCity("Pari").setLat(48194386).setLon(2413386).setName("bot1").build());
+        add(botBuilder.setCountry("France").setCity("Pari").setLat(48294386).setLon(2423386).setName("bot1").build());
+        add(botBuilder.setCountry("France").setCity("Pari").setLat(48394386).setLon(2433386).setName("bot1").build());
     }
 
     public void add(User user) {
@@ -103,6 +107,18 @@ public class HashMapDB {
 
         public BotBuilder setLon(Integer lon) {
             this.lon = lon;
+            return this;
+        }
+
+        public BotBuilder setLat(Double lat) {
+            lat *= 1000000;
+            this.lat = lat.intValue();
+            return this;
+        }
+
+        public BotBuilder setLon(Double lon) {
+            lon *= 1000000;
+            this.lon = lon.intValue();
             return this;
         }
 
--- src/main/java/gr/ru/dao/UtfMySQLDialect.java
+++ src/main/java/gr/ru/dao/UtfMySQLDialect.java
@@ -5,6 +5,6 @@ import org.hibernate.dialect.MySQLDialect;
 public class UtfMySQLDialect extends MySQLDialect {
     @Override
     public String getTableTypeString() {
-        return " DEFAULT CHARSET=utf8";
+        return " DEFAULT CHARSET=utf8mb4";
     }
 }
